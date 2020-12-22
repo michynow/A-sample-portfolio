e240a3b0b4119f9c5f1618f5b154b9dcb8579468
Michal Nowakowski <michy.now@gmail.com>
2020-12-22 15:10:32 +0100

    UI and functionalities update

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
-import getWow from "./getWow.js";
-const hamburgerCont = document.querySelector(".nav-menu__hamburger-container");
-const hamburgerIcon = document.querySelector(".bar1");
-const navMenu = document.querySelector(".nav-menu__links");
-let isMenuEnabled = false;
+const $hamburgerCont = $(".nav-menu__hamburger-container");
+const $hamburgerIcon = $(".hamburger-bar1");
+const $navMenu = $(".nav-menu__links");
+const $width = $(window).width();
+let $isMenuEnabled = false;
 function closeOpenNav() {
-  if (!isMenuEnabled) {
-    navMenu.style.marginLeft = "0";
-    navMenu.style.opacity = "1";
-    isMenuEnabled = true;
-    hamburgerIcon.setAttribute("class", "toggle");
+  if (!$isMenuEnabled) {
+    $navMenu.toggleClass("nav-menu__links--open nav-menu__links");
+    $hamburgerIcon.toggleClass("nav-menu--open hamburger-bar1");
+    $isMenuEnabled = true;
   } else {
-    navMenu.style.marginLeft = "-1000px";
-    hamburgerIcon.setAttribute("class", "bar1");
-    navMenu.style.opacity = "0";
-    isMenuEnabled = false;
+    $navMenu.toggleClass("nav-menu__links nav-menu__links--open");
+    $hamburgerIcon.toggleClass("hamburger-bar1 nav-menu--open");
+    $isMenuEnabled = false;
   }
 }
-hamburgerCont.addEventListener("click", closeOpenNav);
-window.addEventListener("resize", () => {
-  if (window.innerWidth > 768) {
-    navMenu.style.marginLeft = "0";
-    navMenu.style.opacity = "1";
-    isMenuEnabled = true;
-    links.forEach((link) => {
-      link.removeEventListener("click", closeOpenNav);
-    });
-  } else if (window.innerWidth < 768) {
-    navMenu.style.marginLeft = "-1000px";
-    navMenu.style.opacity = "0";
-    isMenuEnabled = false;
-    links.forEach((link) => {
-      link.addEventListener("click", closeOpenNav);
-    });
+$hamburgerCont.on("click", closeOpenNav);
+const $links = $(".nav-menu__link");
+$(window).on("resize", function () {
+  if ($width > 768) {
+    $navMenu.attr("class", "nav-menu__links");
+    $isMenuEnabled = true;
+    $links.off("click", closeOpenNav);
+  } else if ($width < 768) {
+    $navMenu.attr("class", "nav-menu__links");
+    $hamburgerIcon.attr("class", "hamburger-bar1");
+    $isMenuEnabled = false;
   }
 });
-const links = document.querySelectorAll(".nav-menu__link");
+$links.on("click", function () {
+  if ($width < 768) {
+    $navMenu.attr("class", "nav-menu__links");
+    $hamburgerIcon.attr("class", "hamburger-bar1");
+  }
+});
 
 //slick
 $(document).ready(function () {
+  new WOW().init();
   $(".my-projects__slider").slick({
     arrows: true,
     autoplay: false,
